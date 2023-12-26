d1a2ced90ad6502c78dcd825006ef85d1ad661ab
Василий <vasilij.golnev@yandex.ru>
2023-12-26 19:29:35 +0700

    first commit

---
+++ sapce_and_call.py
@@ -0,0 +1,13 @@
+# -*- config: utf-8 -*-
+
+def test():
+    part1 = 'name'
+    part2 = 'space'
+    print(f'Название моего проекта - {part1}{part2}')
+
+test()
+
+def test2(x, y, z):
+    print(f'Координатные оси: abscissa axis = {x}, ordinate axis = {y}, applicate axis = {z}')
+
+test2(x=10, y=20, z=41)
